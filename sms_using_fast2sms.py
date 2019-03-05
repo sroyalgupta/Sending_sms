36159847d45b6573aeda759b52a238945630f30f
Sagar Gupta <sroyalgupta@gmail.com>
2019-03-05 06:58:03 -0800

    Sending a text message to any mobile number using python

---
+++ sms_using_fast2sms.py
@@ -0,0 +1,18 @@
+import requests
+
+url = "https://www.fast2sms.com/dev/bulk"
+
+number=input('Enter your number :')
+message=input('Enter your message :')
+
+querystring = {"authorization":"ZM2aEdmsy3WHNI8xejK6kiJ4hCYrBuwfn9t5QSLpov0VRb7lcP0qHGS5fkgWtPNX2YhFrQy9JnBOZTD6","sender_id":"FSTSMS",
+               "message":message,"language":"english","route":"p",
+               "numbers":number}
+
+headers = {
+    'cache-control': "no-cache"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
